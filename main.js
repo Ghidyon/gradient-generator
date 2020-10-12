8c880b68a8b7d5f9b5a061eb4ee9e61b1fb87614
Ghidyon <gideonakunana@gmail.com>
2020-10-12 21:50:04 +0100

    added button functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const firstColor = document.querySelector('#color1');
 const secondColor = document.querySelector('#color2');
 const cssProperty = document.querySelector('#css-property');
+const copyButton = document.querySelector('#copy-button');
 const body = document.querySelector('body');
 body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value}`;
 cssProperty.textContent = body.style.background;
@@ -10,5 +11,16 @@ const setGradient = () => {
     cssProperty.textContent = body.style.background;
 }
 
+const copyCSS = () => {
+    const copiedText = cssProperty.textContent;
+    const element = document.createElement("textarea");
+    body.appendChild(element);
+    element.value = copiedText;
+    element.select();
+    document.execCommand("copy");
+    body.removeChild(element);
+}
+
 firstColor.addEventListener("input", setGradient);
 secondColor.addEventListener("input", setGradient);
+copyButton.addEventListener("click", copyCSS);
